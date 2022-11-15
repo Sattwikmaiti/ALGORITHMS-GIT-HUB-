52721638aa1bcddf1c3d25fe81f811317fd67583
sattwik <maitisattwik@gmail.com>
2022-11-15 23:42:33 +0530

    h

---
+++ Graph Algorithms/Dijkstra_algorithm.cpp
@@ -0,0 +1,37 @@
+	#include<bits/stdc++.h>
+    using namespace std;
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+  int main()
+  {
+    //vector pass karo bass 
+  }
+    vector <int> dijkstra(int n, vector<vector<int>> graph[], int s)
+    {
+        // Code here
+        // infinity distance 
+          vector<int> dis(n,1e9);
+          //min heap 
+         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq; 
+          dis[s]=0;
+          pq.push({0,s});
+          while(!pq.empty())
+          {
+             pair<int,int>  z=pq.top();
+              pq.pop();
+              int dist=z.first;
+              for(vector<int> child : graph[z.second])
+               {  // checking whether the distance 
+               //to reach child[0]  from z.second is shorter than earlier distance
+                   if(dis[child[0]]>=(dist+child[1]))
+                      {
+                          dis[child[0]]=(dist+child[1]);
+                          pq.push({dis[child[0]],child[0]});
+                      }
+                   
+               }
+              
+              
+          }
+          return dis;
+    }
