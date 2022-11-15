d5ee47d392349b72ab403ec634d041371470fc5f
sattwik <maitisattwik@gmail.com>
2022-11-15 09:52:09 +0530

    h

--- Sorting Techniques/DNF_sort.cpp
+++ Sorting Techniques/DNF_sort.cpp
@@ -9,6 +9,27 @@ int main()
         cin>>a[i];
 
 
+int l=0,m=0,h=n-1;
+while(m<=h)
+{
+
+    if(a[m] ==1)
+       m++;
+       else if(a[m]==0)
+          {
+            swap(a[m],a[l]);
+            l++;
+            m++;
+          }
+          else{
+            swap(a[m],a[h]);
+            h--;
+
+          }
+}
+
+for(int x: a)
+ cout<<x<<" ";
 
 
 }
